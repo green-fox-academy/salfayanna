18554ad178b10d427d7ab2acb89f47c87ce8afe1
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-29 15:35:21 +0100

    fizzbuzz added

--- week-01/day-03/fizz-buzz.ts
+++ week-01/day-03/fizz-buzz.ts
@@ -4,13 +4,19 @@ export {};
 let x: number = 1
 
 for (x; x<= 100; x++) {
-    if (x % 3 === 0) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        
+        console.log("FizzBuzz");
 
-        console.log("Fizz");
     } else if (x % 5 === 0) {
 
         console.log("Buzz");
-    } else {
+
+    } else if (x % 3 === 0) {
+
+        console.log("Fizz");
+
+     } else {
 
         console.log(x)
     } }
