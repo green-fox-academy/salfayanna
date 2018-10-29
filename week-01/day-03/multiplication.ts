fab7d5820c4fe415b494f6999556c91d83585c1c
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-29 14:55:38 +0100

    multiply 9 with 10 and write it out

---
+++ week-01/day-03/multiplication.ts
@@ -0,0 +1,11 @@
+'use strict';
+export {};
+
+let number: number = 9
+let multiplier: number = 1
+
+for (number; multiplier <= 10; multiplier++) {
+
+console.log(multiplier + " * " + number + " = " + multiplier*number);
+    
+}
