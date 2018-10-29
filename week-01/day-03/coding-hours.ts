a976511f56203ac5af9419a5b1000dd7300fd742
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-29 11:19:40 +0100

    Coding Hours math

---
+++ week-01/day-03/coding-hours.ts
@@ -0,0 +1,12 @@
+/*
+Print how many hours is spent with coding in a semester by an attendee,
+if the attendee only codes on workdays.
+*/
+
+console.log (17*5*6);
+
+/*
+Print the percentage of the coding hours in the semester if the average
+work hours weekly is 52*/
+
+console.log ((17 * 5 * 6) / (17 * 52 / 100));t
