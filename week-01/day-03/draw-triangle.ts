b0f8d3704fa817715a7259c7ecd3e7e8ff3516d1
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-29 16:06:05 +0100

    triangle

---
+++ week-01/day-03/draw-triangle.ts
@@ -0,0 +1,13 @@
+'use strict';
+export {};
+
+let lineCount: number = 4;
+let string = "*";
+let counter = 0;
+
+for (counter = 0; counter < lineCount; counter++) {
+
+console.log(string);
+string = string + "*"
+
+}
