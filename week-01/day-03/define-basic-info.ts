b4462d62f5fd43a0d339bfc10df61d6a09d4a3c1
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-29 12:12:22 +0100

    Basic info

---
+++ week-01/day-03/define-basic-info.ts
@@ -0,0 +1,9 @@
+let Myname: string = "Anna Rita Salfay"
+let Myage: number = 29
+let Myheight: number = 1.66
+let Married: boolean = false
+
+console.log ("My name is " + Myname + "."); // Write out my name.
+console.log ("I am " + Myage + " years old."); //Write out my age.
+console.log ("My height is " + Myheight + " cm."); //Write out my height.
+console.log ("Am I married? " + Married); // Marrige status.
