7fa505d1f837e9264a9ee743ac794e48bbe74cec
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-30 12:37:24 +0100

    double number

---
+++ week-01/day-04/doubling.ts
@@ -0,0 +1,18 @@
+'use strict';
+export {};
+
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+let baseNum: number = 123;
+
+function doubling (number) {
+
+var doubledNum:number = number * 2;
+return doubledNum
+
+}
+
+
+console.log(doubling(baseNum));
