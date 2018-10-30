2a91a0af69352d63e35bba9a356e5abb5c5bcc66
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-30 15:11:46 +0100

    print out arrays third element

--- week-01/day-04/third.ts
+++ week-01/day-04/third.ts
@@ -5,3 +5,6 @@ export {};
 //    with the following content: `[1, 3, 5, 7]`
 // -  Print the third element of `magicNumbers`
 
+let magicNumbers: number[] = [1, 3, 5, 7];
+
+console.log(magicNumbers[2]);
