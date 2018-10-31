135940aa886b5d35aa01cb1e65f524fa0678c390
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-31 15:09:19 +0100

    sum all number

--- week-01/day-04/sum-all.ts
+++ week-01/day-04/sum-all.ts
@@ -1,14 +1,24 @@
 'use strict';
-export {};
+export { };
 
 // -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // -  Log the sum of the elements in `ai` to the console
 
 let ai: number[] = [3, 4, 5, 6, 7];
 
-let sum = 0;
-for(let i=0; i < ai.length; i++) { 
- sum = sum + ai[i]
+/*let sum = 0;
+for (let i = 0; i < ai.length; i++) {
+  sum = sum + ai[i]
 }
+
+console.log(sum);*/
+
+// ezt kéne valahogy forEach-el megoldani
+
+let sum = 0;
+ai.forEach(function (element) {
+    sum += element;
+}
+)
 
 console.log(sum);
