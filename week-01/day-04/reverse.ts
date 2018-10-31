53380cc6bd6644a0dd170904444d1162112fffc5
Salfay Anna Rita <salfay.anna@gmail.com>
2018-10-31 12:00:31 +0100

    reverse array with method and loop

---
+++ week-01/day-04/reverse.ts
@@ -0,0 +1,22 @@
+'use strict';
+export { };
+
+// -  Create a variable named `numList`
+//    with the following content: `[3, 4, 5, 6, 7]`
+// -  Reverse the order of the elements in `numList`
+// 	   -  do it with the built in method
+//	   -  do it with creating a new temp array and a loop
+// -  Print the elements of the reversed `numList`
+
+let numList: number[] = [3, 4, 5, 6, 7];
+
+let numlistReversed = numList.reverse();
+console.log(numlistReversed);
+//rev
+
+let index = numList.length
+
+for (index = numList.length - 1; index > -1; index--) {
+  console.log(numList[index]);
+}
+//rev with loop
