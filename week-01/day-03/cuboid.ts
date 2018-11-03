93c5979607005a3f8206148d60d1d37fdc3585ac
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 10:38:45 +0100

    Fixed code formatting

--- week-01/day-03/cuboid.ts
+++ week-01/day-03/cuboid.ts
@@ -1,12 +1,12 @@
 'use strict';
-export {};
+export { };
 
 let a: number = 10
 let b: number = 12
 let c: number = 16
 
-let V = a*b*c
-let A = 2*(a*b + a*c + b*c)
+let V = a * b * c
+let A = 2 * (a * b + a * c + b * c)
 
 console.log("Surface Area: " + A);
 console.log("Volume: " + V);
