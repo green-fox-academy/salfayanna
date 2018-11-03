11c7e4da2b42ec6e60ddd53f7e4fcea4f1c77328
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 10:22:27 +0100

    Fixed code formatting

--- week-01/day-03/bmi.ts
+++ week-01/day-03/bmi.ts
@@ -1,7 +1,7 @@
 'use strict';
-export {};
+export { };
 
 let massInKg: number = 81.2;
 let heightInM: number = 1.78;
 
-console.log ("Body mass index: " + (massInKg/heightInM)); // Print BMI.
+console.log("Body mass index: " + (massInKg / heightInM)); // Print BMI.
