fcc9515d0452d0dd7c613fa0293d131060593e5f
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 17:01:11 +0100

    Draw diamond

--- week-01/day-03/draw-diamond.ts
+++ week-01/day-03/draw-diamond.ts
@@ -1,15 +1,38 @@
 'use strict';
-export {};
+export { };
 
 let lineCount: number = 7;
 
-  for (var i = 0; i < lineCount / 2; i++) {
-    var str = '';
-    for (var j = 1; j < lineCount-i/; j++) {
-      str = str + ' ';
-    }
-    for (var k = 1; k <= (2*i+1); k++) {
-      str = str + '*';
-    }
-    console.log(str);
+/*for (let i = 0; i < lineCount; i++) {
+  let str = '';
+  for (let j = 1; j < lineCount - i; j++) {
+    str = str + ' ';
   }
+  for (let k = 1; k <= (2 * i + 1); k++) {
+    str = str + '*';
+  }
+  console.log(str);
+}*/
+
+for (let row = 0; row < lineCount; row++) {
+  let strg: string = '';
+  if (row < lineCount / 2) {
+    for (let j = 1; j < lineCount - row; j++) {
+      strg = strg + ' ';
+    }
+    for (let k = 0; k <= (2 * row); k++) {
+      strg = strg + '*';
+    }
+  } else if (row === lineCount / 2) {
+    strg = '*'.repeat(lineCount);
+  } else {
+    for (let k = 1; k <= (row); k++) {
+      strg = strg + ' ';
+    }
+    for (let j = 0; j < 2 * (lineCount - row) - 1; j++) {
+      strg = strg + '*';
+    }
+  }
+  console.log(strg);
+}
+
