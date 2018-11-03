cf894eb379672d6597ca72fe724d8da1d6146578
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 17:11:38 +0100

    Calculate coding hours

--- week-01/day-03/coding-hours.ts
+++ week-01/day-03/coding-hours.ts
@@ -1,15 +1,22 @@
 'use strict';
-export {};
+export { };
 
-/*
-Print how many hours is spent with coding in a semester by an attendee,
-if the attendee only codes on workdays.
-*/
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
 
-console.log (17*5*6);
+let dailyHours: number = 6;
+let semesterLength: number = 17;
+let workDays: number = 5;
+let avgWorkHoursWeekly: number = 52;
 
-/*
-Print the percentage of the coding hours in the semester if the average
-work hours weekly is 52*/
+let codingHoursSemester: number = semesterLength * workDays * dailyHours;
 
-console.log ((17 * 5 * 6) / (17 * 52 / 100));t
+console.log(codingHoursSemester);
+
+console.log(codingHoursSemester / (semesterLength * avgWorkHoursWeekly) * 100);
