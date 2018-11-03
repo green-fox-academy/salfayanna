eeca4183264b865ce12c44be0fadc7d4f8b28efa
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 10:43:38 +0100

    Fixed code formartting

--- week-01/day-03/draw-pyramid.ts
+++ week-01/day-03/draw-pyramid.ts
@@ -1,19 +1,19 @@
 'use strict';
-export {};
+export { };
 
 let lineCount: number = 4;
 
 /*for (var line = "#"; line.length < 8; line += "#")
   console.log(line); ez háromszöget csinál csak sokkal
   rövidebben mint én az előző feladatban*/
-  
-  for (var i = 0; i < lineCount; i++) {
-    var str = '';
-    for (var j = 1; j < lineCount-i; j++) {
-      str = str + ' ';
-    }
-    for (var k = 1; k <= (2*i+1); k++) {
-      str = str + '*';
-    }
-    console.log(str);
+
+for (let i = 0; i < lineCount; i++) {
+  let str = '';
+  for (let j = 1; j < lineCount - i; j++) {
+    str = str + ' ';
   }
+  for (let k = 1; k <= (2 * i + 1); k++) {
+    str = str + '*';
+  }
+  console.log(str);
+}
