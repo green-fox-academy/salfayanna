3658b5041efa6161b1ce56c94e6afb49a2121507
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 11:34:17 +0100

    draw chess table

--- week-01/day-03/draw-chess-table.ts
+++ week-01/day-03/draw-chess-table.ts
@@ -1,2 +1,26 @@
 'use strict';
 export {};
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+let rowOdd: string = ' %';
+let rowEven: string = '% ';
+let rowCount: number = 8;
+
+for (let line: number = 0; line < rowCount; line++) {
+  if( line % 2 === 0) {
+    console.log(rowEven.repeat(rowCount / 2));
+  } else {
+    console.log(rowOdd.repeat(rowCount / 2));
+  }
+}
