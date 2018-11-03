ac3f51f2b54ca25b4c0a27737e000a6156fc0ca2
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 15:13:08 +0100

    Draw diagonal square

--- week-01/day-03/draw-diagonal.ts
+++ week-01/day-03/draw-diagonal.ts
@@ -1,10 +1,6 @@
 'use strict';
 export { };
 
-let lineCount: number = 6;
-let empty = " "
-let fill = '%'
-
 // Write a program that draws a
 // square like this:
 //
@@ -17,31 +13,26 @@ let fill = '%'
 //
 // The square should have as many lines as lineCount is
 
+let lineCount: number = 6;
+let empty = " "
+let fill = '%'
+
 for (let row = 0; row < lineCount; row++) {
-
   if (row === 0 || row === lineCount - 1) {
-
     console.log(fill.repeat(lineCount));
-
   } else {
+    let strg: string = '';
+    strg = fill + strg;
 
-    console.log(fill);
-
-    for (let s = 1; s < lineCount - 1; s++) {
-
-      if (s === row) {
-
-        console.log(fill);
-
+    for (let col = 1; col < lineCount - 1; col++) {
+      if (col === row) {
+        strg = strg + fill;
       } else {
-
-        console.log(empty);
-
+        strg = strg + empty;
       }
-
     }
-    console.log(fill);
 
+    strg = strg + fill;
+    console.log(strg);
   }
-
 }
