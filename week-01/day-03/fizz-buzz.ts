b1243282147883844835d577392d115602e3106c
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-03 11:01:56 +0100

    Fixed code formatting

--- week-01/day-03/fizz-buzz.ts
+++ week-01/day-03/fizz-buzz.ts
@@ -1,22 +1,16 @@
 'use strict';
-export {};
+export { };
 
 let x: number = 1
 
-for (x; x<= 100; x++) {
+for (x; x <= 100; x++) {
     if (x % 3 === 0 && x % 5 === 0) {
-        
         console.log("FizzBuzz");
-
     } else if (x % 5 === 0) {
-
         console.log("Buzz");
-
     } else if (x % 3 === 0) {
-
         console.log("Fizz");
-
-     } else {
-
+    } else {
         console.log(x)
-    } }
+    }
+}
