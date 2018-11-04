53ccd08e303dc90cfd3130316263918901513647
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-04 18:39:58 +0100

    not ready yet

--- week-01/day-03/ParametricAverage.ts
+++ week-01/day-03/ParametricAverage.ts
@@ -1,2 +1,22 @@
 'use strict';
-export {};
+export { };
+
+// Write a program that asks for a number.
+// It would ask this many times to enter an number,
+// if all the numbers are entered, it should print the sum and average of these
+// numbers like:
+//
+// Sum: 22, Average: 4.4
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Please enter count of numbers to work with: ', (count: number) => {
+  console.log(` ${count}`);
+
+  rl.close();
+});
