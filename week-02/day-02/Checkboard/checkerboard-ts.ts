66ccd9d1780e629e38edaa3cb39ad24b950f84a1
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-07 16:44:53 +0100

    draw chessboard

--- week-02/day-02/Checkboard/checkerboard-ts.ts
+++ week-02/day-02/Checkboard/checkerboard-ts.ts
@@ -6,3 +6,16 @@ const ctx = canvas.getContext('2d');
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Fill the canvas with a checkerboard pattern.
+
+let side: number = 25;
+let chessBoard_X: number = 0;
+let chessBoard_Y: number = 0;
+
+for (let i = 0; i < canvas.width / side; i++) {
+  for (let j = 0; j < canvas.height / side; j++) {
+    if ((i+j) % 2 === 0) {
+      ctx.fillStyle = 'black';
+      ctx.fillRect(i*side, j*side, side, side);
+    }
+  }
+}
