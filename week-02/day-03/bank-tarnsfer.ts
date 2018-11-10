f6e24d7e986bb64bb547e8b96d320a4e14890904
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-10 21:10:45 +0100

    money transfer added

--- week-02/day-03/bank-tarnsfer.ts
+++ week-02/day-03/bank-tarnsfer.ts
@@ -27,27 +27,22 @@ function transferAmount(array: any[], fromAccountNumber, toAccountNumber, amount
   let to: any = array.find(function (element) {
     return element.accountNumber === toAccountNumber;
   });
-  if(from && to && from.balance - amountOfCash >= 0) {
-    from.balance - amountOfCash;
-    to.balance + amountOfCash;
+  if (from && to && from.balance - amountOfCash >= 0) {
+    from.balance -= amountOfCash;
+    to.balance += amountOfCash;
     console.log(accounts);
-  } else if (!from || !to){
+  } else if (!from || !to) {
     console.log('404 - account not found');
+  } else if (from.balance - amountOfCash < 0) {
+    console.log('Not enough cash')
   }
+  return accounts;
 }
 
 transferAmount(accounts, 23456311, 43546731, 500);
 
-// transferAmount(accounts, 43546731, 23456311, 500.0);
-//After printing the "accounts" it should look like:
-// const accounts = [
-//	{ clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
-//	{ clientName: 'Vladimir', accountNumber: 43546731, balance: 5204099571.23 },
-//	{ clientName: 'Sergei', accountNumber: 23456311, balance: 1354100.0 }
-//]
-
-// export = {
-//   getNameAndBalance,
-//   transferAmount,
-//   accounts
-// };
+export = {
+  getNameAndBalance,
+  transferAmount,
+  accounts
+};
