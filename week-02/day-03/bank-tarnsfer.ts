1d260fa9361b20a6810b5e91c4688e106df788b1
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-10 20:24:28 +0100

    get name and baÃlance

---
+++ week-02/day-03/bank-tarnsfer.ts
@@ -0,0 +1,48 @@
+'use strict';
+export { };
+
+const accounts: any[] = [
+  { clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
+  { clientName: 'Vladimir', accountNumber: 43546731, balance: 5204100071.23 },
+  { clientName: 'Sergei', accountNumber: 23456311, balance: 1353600.0 }
+];
+
+function getNameAndBalance(accountNumber: number) {
+  let found = accounts.find(function(element) {
+    return element.accountNumber === accountNumber;
+  });
+  if(found){
+    console.log(`${found.clientName}, ${found.balance}`);
+  } else {
+    console.log('404 - account not found')
+  }
+}
+
+getNameAndBalance(11234543);
+
+// Create function that returns the name and balance of cash on an account in a list
+// getNameAndBalance(11234543);
+// should return: ['Igor', 203004099.2]
+
+// Create function that transfers an amount of cash from one account to another
+// it should have four parameters:
+//  - the accounts
+//  - from accountNumber
+//  - to accountNumber
+//  - amount of cash to transfer
+//
+// Log "404 - account not found" if any of the account numbers don't exist to the console.
+
+// transferAmount(accounts, 43546731, 23456311, 500.0);
+//After printing the "accounts" it should look like:
+// const accounts = [
+//	{ clientName: 'Igor', accountNumber: 11234543, balance: 203004099.2 },
+//	{ clientName: 'Vladimir', accountNumber: 43546731, balance: 5204099571.23 },
+//	{ clientName: 'Sergei', accountNumber: 23456311, balance: 1354100.0 }
+//]
+
+// export = {
+//   getNameAndBalance,
+//   transferAmount,
+//   accounts
+// };
