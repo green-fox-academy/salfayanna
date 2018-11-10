b22b5092b52667b1c2fecbe76f8649acf9541e61
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-10 18:45:34 +0100

    draw envelope star with variable top and bottom color

--- week-02/day-04/Envelope_star/envelope-star.ts
+++ week-02/day-04/Envelope_star/envelope-star.ts
@@ -3,24 +3,32 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-let stepFrequency = 10;
-let maxWidth = canvas.width / 2;
-let maxWidth2 = canvas.width / 2;
 
-function drawEnvelopeStar(startVertical: number, startHorizontalLeft: number, startHorizontalRight: number, color: string) {
-ctx.strokeStyle = color;
-ctx.beginPath();
-ctx.moveTo(canvas.width / 2, startVertical);
-ctx.lineTo(startHorizontalLeft, canvas.height / 2);
-ctx.moveTo(canvas.width / 2, startVertical);
-ctx.lineTo(startHorizontalRight, canvas.height / 2);
-ctx.stroke();
+function drawTwoLines(startVertical: number, startHorizontalLeft: number, startHorizontalRight: number, color: string) {
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, startVertical);
+  ctx.lineTo(startHorizontalLeft, canvas.height / 2);
+  ctx.moveTo(canvas.width / 2, startVertical);
+  ctx.lineTo(startHorizontalRight, canvas.height / 2);
+  ctx.stroke();
 }
 
+function drawEnvelopeStar(stepFrequency: number, colorTop: string, colorBottom: string) {
+  let maxWidth = canvas.width / 2;
+  let maxWidth2 = canvas.width / 2;
 
-for (let i = 0; i < canvas.width / 2 / stepFrequency; i++) {
-  drawEnvelopeStar(i * stepFrequency, maxWidth, maxWidth2, 'purple')
-  maxWidth += stepFrequency;
-  maxWidth2 -= stepFrequency;
+  for (let i = 0; i < canvas.width / 2 / stepFrequency; i++) {
+    drawTwoLines(i * stepFrequency, maxWidth, maxWidth2, colorTop);
+    drawTwoLines(canvas.width - i * stepFrequency, maxWidth, maxWidth2, colorBottom);
+    maxWidth += stepFrequency;
+    maxWidth2 -= stepFrequency;
+  }
 }
+
+drawEnvelopeStar(6, 'aquamarine', 'indianred' );
+
+
+
+
 
