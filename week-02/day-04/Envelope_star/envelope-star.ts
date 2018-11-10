63a9ff38e7b108062b1db06cecae8e775a7fd73a
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-10 17:50:06 +0100

    draw half star from top

--- week-02/day-04/Envelope_star/envelope-star.ts
+++ week-02/day-04/Envelope_star/envelope-star.ts
@@ -4,33 +4,23 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 let stepFrequency = 10;
-let maxHeight = canvas.height / 2;
-let maxHeight2 = canvas.height / 2;
+let maxWidth = canvas.width / 2;
+let maxWidth2 = canvas.width / 2;
 
-function drawEnvelopeStar(startHorizontal: number, startVertivalUp: number, startVerticalDown: number, color: string) {
+function drawEnvelopeStar(startVertical: number, startHorizontalLeft: number, startHorizontalRight: number, color: string) {
 ctx.strokeStyle = color;
 ctx.beginPath();
-ctx.moveTo(startHorizontal, canvas.height / 2);
-ctx.lineTo(canvas.width / 2, startVertivalUp);
-ctx.moveTo(startHorizontal, canvas.height / 2);
-ctx.lineTo(canvas.width / 2, startVerticalDown);
+ctx.moveTo(canvas.width / 2, startVertical);
+ctx.lineTo(startHorizontalLeft, canvas.height / 2);
+ctx.moveTo(canvas.width / 2, startVertical);
+ctx.lineTo(startHorizontalRight, canvas.height / 2);
 ctx.stroke();
 }
 
 
-for (let i = 0; i < canvas.height / 2 / stepFrequency; i++) {
-  drawEnvelopeStar(i * stepFrequency, maxHeight, maxHeight2, 'purple')
-  maxHeight += stepFrequency;
-  maxHeight2 -= stepFrequency;
+for (let i = 0; i < canvas.width / 2 / stepFrequency; i++) {
+  drawEnvelopeStar(i * stepFrequency, maxWidth, maxWidth2, 'purple')
+  maxWidth += stepFrequency;
+  maxWidth2 -= stepFrequency;
 }
 
-// for (let i = 0; i < canvas.height / 2 / stepFrequency; i++) {
-//   ctx.beginPath();
-//   ctx.moveTo(0, canvas.height / 2);
-//   ctx.lineTo(canvas.width / 2, maxHeight2);
-//   ctx.moveTo(0, canvas.height / 2);
-//   ctx.lineTo(canvas.width / 2, maxHeight);
-//   ctx.stroke();
-//   maxHeight -= stepFrequency;
-//   maxHeight2 += stepFrequency;
-// }
