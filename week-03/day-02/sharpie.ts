a348e13660d996e2088ac5f4fe5aff204cdcb975
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-13 11:58:44 +0100

    inkAmount decrease

---
+++ week-03/day-02/sharpie.ts
@@ -0,0 +1,20 @@
+'use strict';
+export { };
+
+class Sharpie {
+  color: string
+  width: number
+  inkAmount: number = 100;
+
+  constructor(color: string, width: number) {
+    this.color = color;
+    this.width = width;
+  }
+
+  use() {
+    if (this.inkAmount > 0) {
+      this.inkAmount -= 1;
+    }
+  }
+}
+
