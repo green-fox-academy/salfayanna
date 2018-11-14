4d71d6afb6a7f3899a1280f2d194b9ecc9f3eb57
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-14 11:34:54 +0100

    inkAmount default chnaged

--- week-03/day-02/sharpie.ts
+++ week-03/day-02/sharpie.ts
@@ -2,13 +2,14 @@
 export { };
 
 export class Sharpie {
-  color: string
-  width: number
-  inkAmount: number = 100;
+  color: string;
+  width: number;
+  inkAmount: number;
 
-  constructor(color: string, width: number) {
+  constructor(color: string, width: number, inkAmount: number = 100) {
     this.color = color;
     this.width = width;
+    this.inkAmount = inkAmount;
   }
 
   use() {
