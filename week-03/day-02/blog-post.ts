65ca10960a332c825dc8152ee2348bb8188a9451
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-14 17:05:37 +0100

    export added

--- week-03/day-02/blog-post.ts
+++ week-03/day-02/blog-post.ts
@@ -1,5 +1,4 @@
 'use strict';
-export { };
 
 class BlogPost {
 
@@ -20,4 +19,6 @@ let post1 = new BlogPost('John Doe', 'Lorem Ipusum', 'Lorem ipsum dolor sit amet
 let post2 = new BlogPost('Tim Urban', 'Wait but why', 'A popular long-form, stick-figure-illustrated blog about almost everything.', '2010.10.10.');
 let post3 = new BlogPost('William Turton', 'One Engineer Is Trying to Get IBM to Reckon With Trump', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.', '2017.03.28.');
 
-console.log(post1, post2, post3);
+
+export {BlogPost, post1, post2, post3};
+//console.log(post1, post2, post3);
