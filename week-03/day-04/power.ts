96c84deaa70e13348dd44d5102ec780d96fa6dbe
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-15 16:46:30 +0100

    pow number

---
+++ week-03/day-04/power.ts
@@ -0,0 +1,15 @@
+'use struct'
+export { };
+
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+function powerN(baseNumber: number, number: number): number {
+  if ( number <= 0) {
+    return 1;
+  } else {
+    return powerN(baseNumber, number-1) * baseNumber;
+  }
+}
+
+console.log(powerN(3,2));
