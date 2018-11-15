dd6d9e37fcd7c9c042d8acd6f61a23da0cec3c50
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-15 15:18:47 +0100

    list numbers backward

--- week-03/day-04/counter.ts
+++ week-03/day-04/counter.ts
@@ -2,11 +2,17 @@
 export { };
 // Write a recursive function that takes one parameter: n and counts down from n.
 
-function countDown(num: number): void {
-  console.log(num);
-  if (num > 0) {
-    countDown(num-1);
-}
+function countDown(num: number): string | number {
+  let numbers: number = number;
+  if (num === 0) { 
+    return 0;
+  }
+  else {
+    countDown(num - 1);
+    numbers += num;
+    return num;
+  }
+  console.log(numbers);
 }
 
 countDown(10);
