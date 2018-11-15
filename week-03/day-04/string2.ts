d6d0e7d3efdbb6b1cdf35b126b4a1f0f89ef5927
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-15 16:18:54 +0100

    remover all x from string

---
+++ week-03/day-04/string2.ts
@@ -0,0 +1,14 @@
+'use struct'
+export { };
+
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+function xRemover(stringToChange: string): string {
+  if (!stringToChange.includes('x')) {
+    return stringToChange;
+  } else {
+   return xRemover(stringToChange.replace('x', ''));
+  }
+}
+
+console.log(xRemover('xoxoxoxoxo'));
