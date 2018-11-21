907600c582fcfb72ec24b6c6c16ef4ee6a530066
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-21 16:09:26 +0100

    feat(Instrument)play method added

--- week-04/day-03/Instrument/model-stringed-instrument.ts
+++ week-04/day-03/Instrument/model-stringed-instrument.ts
@@ -6,24 +6,52 @@ import { StringedInstrument } from "./stringedinstrument-class";
 class ElectricGuitar extends StringedInstrument {
 
 
-  constructor(numberOfStrings: number = 6, name: string) {
+  constructor(numberOfStrings: number = 6, name: string = 'Electric Guitar') {
     super(numberOfStrings, name)
   }
+
+  sound(): string {
+    return 'Twang';
+  }
+
+  play(): void {
+    return console.log(`${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+
 }
 
 class BassGuitar extends StringedInstrument {
 
 
-  constructor(numberOfStrings: number = 4, name: string) {
+  constructor(numberOfStrings: number = 4, name: string = 'Bass Guitar') {
     super(numberOfStrings, name)
   }
+
+  sound(): string {
+    return 'Duum-duum-duum';
+  }
+
+  play(): void {
+    return console.log(`${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+
 }
 
 class Violin extends StringedInstrument {
 
 
-  constructor(numberOfStrings: number = 4, name: string) {
+  constructor(numberOfStrings: number = 4, name: string = 'Violin') {
     super(numberOfStrings, name)
   }
+
+  sound(): string {
+    return 'Screech';
+  }
+
+  play(): void {
+    return console.log(`${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+
 }
-export {ElectricGuitar, BassGuitar, Violin};
+
+export { ElectricGuitar, BassGuitar, Violin };
