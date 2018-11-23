6692118174ce9c672b4f927deedb4b6cf5f34395
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-23 19:49:39 +0100

    feat(Data structures): table draw function added

--- week-02/day-03/table-printers.ts
+++ week-02/day-03/table-printers.ts
@@ -1,21 +1,6 @@
 'use strict';
 export { };
 
-// Create a function that prints the ingredient list of dictionaries to the console in the following format:
-//
-// +--------------------+---------------+----------+
-// | Ingredient         | Needs cooling | In stock |
-// +--------------------+---------------+----------+
-// | vodka              | Yes           | 1        |
-// | coffee_liqueur     | Yes           | -        |
-// | fresh_cream        | Yes           | 1        |
-// | captain_morgan_rum | Yes           | 2        |
-// | mint_leaves        | No            | -        |
-// +--------------------+---------------+----------+
-//
-// OPTIONAL
-// The frist columns should be automatically as wide as the longest key
-
 const ingredients: any[] = [
   { name: 'vodka', inStock: 1, needsCooling: true },
   { name: 'coffee_liqueur', inStock: 0, needsCooling: true },
@@ -28,27 +13,71 @@ const ingredients: any[] = [
 ];
 
 let names: string[] = [];
-let inStock: number[] = [];
+let inStock: string[] = [];
 let needsCooling: string[] = [];
 
-ingredients.forEach(function (item) {
-  names.push(item.name);
-});
-ingredients.forEach(function (item) {
-  inStock.push(item.inStock);
-});
-ingredients.forEach(function (element) {
-  if(element.needsCooling === true) {
-    needsCooling.push('yes');
-  } else {
-    needsCooling.push('no');
-  }
+function initializeData(array: any[]): void {
+  ingredients.forEach(function (item) {
+    names.push(item.name);
   });
 
-for(let i: number = 0; i < ingredients.length; i++){
-console.log(`${names[i]} ${needsCooling[i]} ${inStock[i]}`);
+  ingredients.forEach(function (item) {
+    if (item.inStock > 0) {
+      inStock.push(`${item.inStock}`)
+    } else {
+      inStock.push('-');
+    }
+  });
+
+  ingredients.forEach(function (element) {
+    if (element.needsCooling) {
+      needsCooling.push('yes');
+    } else {
+      needsCooling.push('no');
+    }
+  });
 }
 
-// console.log(names);
-// console.log(inStock);
-// console.log(needsCooling);
+initializeData(ingredients);
+
+function getLength(array: string[]): number {
+  let biggestLength = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > biggestLength) {
+      biggestLength = array[i].length;
+    }
+  }
+  return biggestLength;
+}
+
+function processColumn(data: string[], column: string[], maxRowLength: number, empty: string): void {
+  data.forEach((e, i, a) => {
+    column.push(`${data[i]}${empty.repeat(maxRowLength - data[i].length)}|`)
+  });
+}
+
+function createTable(): string[] {
+  let empty: string = ' ';
+  let horSep: string = '-';
+  let sepLine: string = `+${horSep.repeat(getLength(names))}+${horSep.repeat(15)}+${horSep.repeat(10)}+`
+  let header: string = `|Ingredient${empty.repeat(getLength(names) - 10)}|Needs cooling${empty.repeat(2)}|In stock${empty.repeat(2)}|`
+  let table: string[] = [];
+  let column1: string[] = [];
+  let column2: string[] = [];
+  let column3: string[] = [];
+
+  table.push(sepLine, header, sepLine);
+
+  processColumn(names, column1, getLength(names), empty);
+  processColumn(needsCooling, column2, 15, empty);
+  processColumn(inStock, column3, 10, empty);
+
+  for (let i: number = 0; i < ingredients.length; i++) {
+    table.push(`|${column1[i]}${column2[i]}${column3[i]}`);
+  }
+  table.push(sepLine);
+  return table;
+}
+
+console.log(createTable());
