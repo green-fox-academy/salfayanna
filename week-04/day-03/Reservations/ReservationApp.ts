20fd9e885c85a9cef6c37c071d2f4d04b82347ae
Salfay Anna Rita <salfay.anna@gmail.com>
2018-11-25 16:23:25 +0100

    feat(Reservation)reservations creater function added

---
+++ week-04/day-03/Reservations/ReservationApp.ts
@@ -0,0 +1,14 @@
+' use strict'
+
+import {Reservation} from "./reservations";
+import {Reservationy} from "./reservationy-interface";
+
+function getBookingNumber(reservation: Reservationy, amountOfBookings: number){
+  for(let i: number = 0; i <= amountOfBookings; i++) {
+   console.log(`Booking# ${reservation.getCodeBooking()} for ${reservation.getDowBooking()}`);   
+  }
+}
+
+let reservations = new Reservation();
+
+getBookingNumber(reservations, 6);
