3b8179c4e80b22a9b6402660325a9953ef1cb7c0
Salfay Anna Rita <salfay.anna@gmail.com>
2018-12-12 13:26:22 +0100

    feat(eventListener): writ out last pressed keys code

--- week-07/day-03/Keyup/keyup.js
+++ week-07/day-03/Keyup/keyup.js
@@ -4,6 +4,6 @@ const globalWindow = document.querySelector('body');
 const h1Element = document.querySelector('h1'); 
 
 globalWindow.addEventListener('keyup', (event) => {
-  const keyName = event.key;
-  h1Element.innerHTML=`Last key pressed: ${keyName}`;
+  const keyName = event.keyCode;
+  h1Element.innerHTML=`Last pressed key code is: ${keyName}`;
 });
