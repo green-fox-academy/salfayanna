e2c86ec66dbc28d4424c9afde2f533ba28f4b92d
Salfay Anna Rita <salfay.anna@gmail.com>
2018-12-30 14:57:45 +0100

    feat(Callbacks): log fruits with delay

---
+++ week-07/day-02/Multiple Timeout/multipleTimeout.js
@@ -0,0 +1,6 @@
+'use strict'
+
+console.log('apple');
+setTimeout(function(){ console.log('pear'); }, 1000);
+setTimeout(function(){ console.log('melon'); }, 3000);
+setTimeout(function(){ console.log('grapes'); }, 5000);
